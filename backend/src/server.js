ce336277e8e4a6cf48708d68d8f2313395f9a18f
Mayank Surti <mayank.surti@uconn.edu>
2025-09-06 11:14:23 -0400

    prep deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
@@ -10,12 +11,15 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
-app.use(express.json()) // middleware (parse JSON bodies req.body)
-app.use(rateLimiter)
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+    app.use(express.json()) // middleware (parse JSON bodies req.body)
+    app.use(rateLimiter)
+}
 
 // app.use((req, res, next) => {
 //     console.log(`Req method is ${req.method} and Req URL is ${req.url}`)
@@ -23,6 +27,14 @@ app.use(rateLimiter)
 // })
 
 app.use("/api/notes", notesRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(5001, () => {
